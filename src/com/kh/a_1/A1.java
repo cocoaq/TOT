e396fe81da511ec5fabcf35870e8b2ab0b0f87cf
인서 <@DESKTOP-J836M3E>
2020-07-23 23:55:11 +0900

    zzz

---
+++ src/com/kh/a_1/A1.java
@@ -0,0 +1,27 @@
+package com.kh.a_1;
+
+public class A1 {
+	public static void main(String[] args) {
+
+		//1번 [변수]
+		System.out.println("키보드로 입력 받은값\n 파일로부터 읽어온 값 \n "
+				+ "네트워크로부터 전송 받은 값 \n"
+				+ " DB서버로부터 읽어온 값");
+		System.out.println("입력버퍼");
+		System.out.println("메모리에 기록");
+		System.out.println("CPU가 읽어서 계산 처리");
+		System.out.println("계산결과를 메모리로 보냄");
+		System.out.println("메모리에 결과값 기록함");
+		System.out.println("화면에 출력함" + "\n 파일에 저장함 \n 네트워크로 전송함 \n"
+				+ "DB서버에 기록함");
+
+		//2번 [연산자]
+		System.out.println("최우선연산자");
+		System.out.println("단항연산자");
+		System.out.println("이항연산자");
+		System.out.println("삼항연산자");
+		System.out.println("대입연산자");
+		System.out.println("나열연산자");
+	}
+
+}
